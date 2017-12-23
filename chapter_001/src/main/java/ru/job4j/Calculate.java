e8342c132755659b6e88068f98b1277eec53dabd
AlexandrMoralev <moralev.alexandr@yandex.ru>
2017-12-23 18:12:58 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,11 @@
 package ru.job4j;
 
+/**
+ *Class Calculate решение задания части 1 урок "1.3. JavaDoc. Code convention [#4087]"
+ *@author 
+ *@since 23.12.2017
+ *@version 1
+ */
 public class Calculate {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
