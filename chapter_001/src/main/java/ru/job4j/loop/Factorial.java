11bce3e9db53745e84a1b7ca915b7d0e00be09d1
AlexandrMoralev <moralev.alexandr@yandex.ru>
2018-01-10 01:16:23 +0300

    4.2. Создать программу вычисляющую факториал. [#193]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,27 @@
+package ru.job4j.loop;
+
+/**
+ * Factorial
+ *
+ * @author Alexandr Moralev (moralev.alexandr@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Factorial {
+
+    /**
+     * Method calc
+     *
+     * @param n - int number, n > 0
+     * @return the factorial of a number n
+     */
+    public int calc(int n) {
+        int result = 1;
+
+        for (int i = 1; i <= n ; i++) {
+            result *= i;
+        }
+
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/FactorialTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * FactorialTest
+ *
+ * @author Alexandr Moralev (moralev.alexandr@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class FactorialTest {
+
+    /**
+     * Test. 5! == 120
+     */
+    @Test
+    public void whenNumberEqualsFiveThen120 () {
+        Factorial f = new Factorial();
+        int result = f.calc(5);
+        assertThat(result, is(120));
+    }
+
+    /**
+     * Test. 0! == 1
+     */
+    @Test
+    public void whenNumberIsZeroThenOne () {
+        Factorial f = new Factorial();
+        int result = f.calc(0);
+        assertThat(result, is(1));
+    }
+}
