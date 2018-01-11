598906d6e4657190889a2346b08f816deaac4e49
AlexandrMoralev <moralev.alexandr@yandex.ru>
2018-01-11 16:53:44 +0300

    5.2. Перевернуть массив. [#4441]

---
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+/**
+ * Turn
+ *
+ * @author Alexandr Moralev (moralev.alexandr@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Turn {
+
+    /**
+     * Method back - invert int[] array
+     * @param array
+     * @return inverted int[] array
+     */
+    public int[] back(int[] array) {
+        int tmp;
+
+        for (int i = 0, j = array.length - 1; i < array.length/2 ; i++, j--) {
+            tmp = array[i];
+            array[i] = array[j];
+            array[j] = tmp;
+        }
+
+        return array;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/TurnTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * TurnTest
+ *
+ * @author Alexandr Moralev (moralev.alexandr@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class TurnTest {
+
+    /**
+     * Test.
+     */
+    @Test
+    public void whenTurnArrayWithEvenAmountOfElementsThenTurnedArray() {
+        Turn turn = new Turn();
+        int[] result = turn.back(new int[] {4, 1, 6, 2});
+        assertThat(result, is(new int[] {2, 6, 1, 4}));
+    }
+
+    /**
+     * Test.
+     */
+    @Test
+    public void whenTurnArrayWithOddAmountOfElementsThenTurnedArray() {
+        Turn turn = new Turn();
+        int[] result = turn.back(new int[] {1, 2, 3, 4, 5});
+        assertThat(result, is(new int[] {5, 4, 3, 2, 1}));
+    }
+}
