6686e798693ecb4d5181b5a1a263a9c185bcedf6
AlexandrMoralev <moralev.alexandr@yandex.ru>
2018-01-22 16:24:19 +0300

    Объединение двух массивов, отсортированных по возрастанию.

---
+++ chapter_001/src/main/java/ru/job4j/exam/SortedArray.java
@@ -0,0 +1,48 @@
+package ru.job4j.exam;
+
+/**
+ * SortedArray
+ *
+ * @author Alexandr Moralev (moralev.alexandr@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class SortedArray {
+
+    /**
+     * Method mergeTwoSortedArrays
+     * @param firstArray first int[] sorted in ascending order
+     * @param secondArray second int[] sorted in ascending order
+     * @return int[] array obtained by merging two input arrays
+     */
+    public int[] mergeTwoSortedArrays(int[] firstArray, int[] secondArray) {
+
+        int[] mergedArray = new int[firstArray.length + secondArray.length];
+        int firstArrIndex = 0;
+        int secondArrIndex = 0;
+        int mergedArrIndex = 0;
+
+        // comparing two input arrays element by element
+        // adding elements to merged array in ascending order
+        // until all the elements of one input array is added to merged array
+        while ((firstArrIndex < firstArray.length) && (secondArrIndex < secondArray.length)) {
+            if (firstArray[firstArrIndex] <= secondArray[secondArrIndex]) {
+                mergedArray[mergedArrIndex++] = firstArray[firstArrIndex++];
+            } else {
+                mergedArray[mergedArrIndex++] = secondArray[secondArrIndex++];
+            }
+        }
+
+        // adding the remaining elements of the second array to merged array
+        while (secondArrIndex < secondArray.length) {
+            mergedArray[mergedArrIndex++] = secondArray[secondArrIndex++];
+        }
+
+        // or adding the rest elements of the first array to merged array
+        while (firstArrIndex < firstArray.length) {
+            mergedArray[mergedArrIndex++] = firstArray[firstArrIndex++];
+        }
+
+        return mergedArray;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/exam/SortedArrayTest.java
@@ -0,0 +1,71 @@
+package ru.job4j.exam;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.core.Is.*;
+
+/**
+ * SortedArrayTest
+ *
+ * @author Alexandr Moralev (moralev.alexandr@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class SortedArrayTest {
+
+    /**
+     * Test. When adding the first array first.
+     */
+    @Test
+    public void whenFirstArrayShorterThanSecondThenOneMergedArray() {
+        SortedArray sortedArray = new SortedArray();
+        int[] result = sortedArray.mergeTwoSortedArrays(
+                new int[]{0, 1, 3, 5},
+                new int[]{2, 4, 5, 6, 7}
+        );
+        int[] expected = {0, 1, 2, 3, 4, 5, 5, 6, 7};
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Test. When adding the second array first.
+     */
+    @Test
+    public void whenSecondArrayShorterThanFirstThenOneMergedArray() {
+        SortedArray sortedArray = new SortedArray();
+        int[] result = sortedArray.mergeTwoSortedArrays(
+                new int[] {-1, 0, 0, 2},
+                new int[] {0, 1, 3}
+        );
+        int[] expected = {-1, 0, 0, 0, 1, 2, 3};
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Test. When adding overlapping elements of equal length arrays
+     */
+    @Test
+    public void whenTwoArraysEqualLengthThenOneMergedArray() {
+        SortedArray sortedArray = new SortedArray();
+        int[] result = sortedArray.mergeTwoSortedArrays(
+                new int[] {1, 3, 5, 8},
+                new int[] {1, 2, 4, 8}
+        );
+        int[] expected = {1, 1, 2, 3, 4, 5, 8, 8};
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Test. Arrays with non-overlapping elements.
+     */
+    @Test
+    public void whenTwoArraysWithNonOverlappingValueRangesThenOneMergedArray() {
+        SortedArray sortedArray = new SortedArray();
+        int[] result = sortedArray.mergeTwoSortedArrays(
+                new int[] {0, 1, 2, 3},
+                new int[] {5, 6, 7}
+        );
+        int[] expected = {0, 1, 2, 3, 5, 6, 7};
+        assertThat(result, is(expected));
+    }
+}
