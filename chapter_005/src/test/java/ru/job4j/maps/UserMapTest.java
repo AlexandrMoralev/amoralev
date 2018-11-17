599c9aac6517a55680b3ba70c1239e8b605ca85c
AlexandrMoralev <moralev.alexandr@yandex.ru>
2018-11-17 18:25:32 +0300

    9. Рассказать и продемонстрировать как переопределяют метод equals.[#78277]

--- chapter_005/src/test/java/ru/job4j/maps/UserMapTest.java
+++ chapter_005/src/test/java/ru/job4j/maps/UserMapTest.java
@@ -21,6 +21,8 @@ public class UserMapTest {
     private UserMap map;
     private User first;
     private User second;
+    private User third;
+    private User fourth;
     private Object[] result;
 
     @Before
@@ -28,17 +30,16 @@ public class UserMapTest {
         this.map = new UserMap();
         this.first = new User("Andrey",
                 0,
-                new GregorianCalendar(2008, 6, 1));
+                new GregorianCalendar(2008, 6, 1)
+        );
 
         this.second = new User("Andrey",
                 0,
-                new GregorianCalendar(2008, 6, 1));
+                new GregorianCalendar(2008, 6, 1)
+        );
         map.add(first);
         map.add(second);
         result = map.getMap().keySet().toArray();
-        for (Object user : result) {
-            System.out.println(user);
-        }
     }
 
     @Ignore
@@ -62,5 +63,44 @@ public class UserMapTest {
     @Test
     public void whenOverrideEqualsAndHashcode() {
         assertThat(result.length, is(1));
+    }
+
+    @Test
+    public void equalsReflexivityTest() {
+        assertThat(first.equals(first), is(true));
+        assertThat(second.equals(second), is(true));
+    }
+
+    @Test
+    public void equalsSymmetryTest() {
+        assertThat(first.equals(second), is(true));
+        assertThat(second.equals(first), is(true));
+    }
+
+    @Test
+    public void equalsTransitivityTest() {
+        third = new User("Andrey",
+                0,
+                new GregorianCalendar(2008, 6, 1)
+        );
+        assertThat(first.equals(second), is(true));
+        assertThat(second.equals(third), is(true));
+        assertThat(third.equals(first), is(true));
+    }
+
+    @Test
+    public void equalsConsistencyTest() {
+        assertThat(first.equals(second), is(true));
+        third = first;
+        fourth = second;
+        assertThat(third.equals(first), is(true));
+        assertThat(fourth.equals(second), is(true));
+        assertThat(third.equals(fourth), is(true));
+    }
+
+    @Test
+    public void whenEqualsForNullReferenceThenReturnsFalse() {
+        assertThat(first.equals(null), is(false));
+        assertThat(second.equals(null), is(false));
     }
 }
