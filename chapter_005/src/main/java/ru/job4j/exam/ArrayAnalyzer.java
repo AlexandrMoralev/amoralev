29366a511f53be73c642e332fab8d53a83291232
AlexandrMoralev <moralev.alexandr@yandex.ru>
2018-12-19 03:16:42 +0300

    Контрольные задания CollectionPro (#3)[#96557] performance fix

--- chapter_005/src/main/java/ru/job4j/exam/ArrayAnalyzer.java
+++ chapter_005/src/main/java/ru/job4j/exam/ArrayAnalyzer.java
@@ -1,9 +1,6 @@
 package ru.job4j.exam;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * ArrayAnalyzer
@@ -18,7 +15,28 @@ public class ArrayAnalyzer<T> {
     // Определить, какие элементы были удалены.
 
     /**
+     * Method findDeletedSlowly - checks for differences between two arrays and finds all removed elements
+     * the complexity of the method is O(n*m)
+     *
+     * @param source T array to compare with
+     * @param edited T array to be checked
+     * @return List of elements removed from the source array, or an EmptyList
+     */
+    public List<T> findDeletedSlowly(final T[] source, final T[] edited) {
+        boolean validated = isArgValid(source) && isArgValid(edited);
+        List<T> src = Collections.EMPTY_LIST;
+        List<T> edit;
+        if (validated) {
+            src = new ArrayList<>(Arrays.asList(source));
+            edit = new ArrayList<>(Arrays.asList(edited));
+            validated = src.removeAll(edit);
+        }
+        return validated ? src : Collections.EMPTY_LIST;
+    }
+
+    /**
      * Method findDeleted - checks for differences between two arrays and finds all removed elements
+     * the complexity of the method is O(n+m)
      *
      * @param source T array to compare with
      * @param edited T array to be checked
@@ -27,10 +45,10 @@ public class ArrayAnalyzer<T> {
     public List<T> findDeleted(final T[] source, final T[] edited) {
         boolean validated = isArgValid(source) && isArgValid(edited);
         List<T> src = Collections.EMPTY_LIST;
-        List<T> edit;
+        Set<T> edit;
         if (validated) {
             src = new ArrayList<>(Arrays.asList(source));
-            edit = new ArrayList<>(Arrays.asList(edited));
+            edit = new HashSet<>(Arrays.asList(edited));
             validated = src.removeAll(edit);
         }
         return validated ? src : Collections.EMPTY_LIST;
--- chapter_005/src/test/java/ru/job4j/exam/ArrayAnalyzerTest.java
+++ chapter_005/src/test/java/ru/job4j/exam/ArrayAnalyzerTest.java
@@ -19,44 +19,136 @@ import static org.junit.Assert.assertThat;
  */
 public class ArrayAnalyzerTest {
 
-    private ArrayAnalyzer<String> analyzer;
+    private ArrayAnalyzer<String> strAnalyzer;
+    private ArrayAnalyzer<Integer> intAnalyzer;
     private final String[] sourceStrArr = {"one", "two", "four", "seven", " "};
     private final String[] editedStrArr = {"one", "four"};
     private final String[] anotherEditedStrArr = {"one", "two", "four", "seven", " "};
     private final String[] zeroStrArr = new String[0];
 
+    private final Integer[] src = new Integer[100000];
+    private final Integer[] edited = new Integer[100000];
+
+    private final String[] srcStr = fillSrcArray(new String[100000]);
+    private final String[] editedStr = fillEditedArray(new String[100000]);
+
     @Before
     public void init() {
-        analyzer = new ArrayAnalyzer<>();
+        strAnalyzer = new ArrayAnalyzer<>();
+        intAnalyzer = new ArrayAnalyzer<>();
+        Arrays.fill(src, 130);
+        Arrays.fill(edited, 1000);
     }
 
+    private String[] fillSrcArray(String[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            arr[i] = "" + i;
+        }
+        return arr;
+    }
+
+    private String[] fillEditedArray(String[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            arr[i] = "" + 2 * i;
+        }
+        return arr;
+    }
+
+    // findDeleted() tests
     @Test
     public void whenThreeElementsDeletedFromArrayShouldReturnListWithDeleted() {
-        assertThat(analyzer.findDeleted(sourceStrArr, editedStrArr), is(new ArrayList<>(Arrays.asList("two", "seven", " "))));
+        assertThat(strAnalyzer.findDeleted(sourceStrArr, editedStrArr), is(new ArrayList<>(Arrays.asList("two", "seven", " "))));
     }
 
     @Test
     public void whenNoneElementsDeletedFromArrayShouldReturnEmptyList() {
-        assertThat(analyzer.findDeleted(sourceStrArr, anotherEditedStrArr), is(Collections.EMPTY_LIST));
+        assertThat(strAnalyzer.findDeleted(sourceStrArr, anotherEditedStrArr), is(Collections.EMPTY_LIST));
     }
 
     @Test
     public void whenFirstArrayIsEmptyShouldReturnEmptyList() {
-        assertThat(analyzer.findDeleted(zeroStrArr, sourceStrArr), is(Collections.EMPTY_LIST));
+        assertThat(strAnalyzer.findDeleted(zeroStrArr, sourceStrArr), is(Collections.EMPTY_LIST));
     }
 
     @Test
     public void whenSecondArrayIsEmptyShouldReturnEmptyList() {
-        assertThat(analyzer.findDeleted(anotherEditedStrArr, zeroStrArr), is(Collections.EMPTY_LIST));
+        assertThat(strAnalyzer.findDeleted(anotherEditedStrArr, zeroStrArr), is(Collections.EMPTY_LIST));
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void whenFirstArrIsNullShouldThrowIAException() {
-        analyzer.findDeleted(null, sourceStrArr);
+        strAnalyzer.findDeleted(null, sourceStrArr);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void whenSecondArrIsNullShouldThrowIAException() {
-        analyzer.findDeleted(sourceStrArr, null);
+        strAnalyzer.findDeleted(sourceStrArr, null);
+    }
+
+
+    // findDeletedSlowly() tests
+    @Test
+    public void whenThreeElementsDeletedFromArrayThenReturnListWithDeleted() {
+        assertThat(strAnalyzer.findDeletedSlowly(sourceStrArr, editedStrArr), is(new ArrayList<>(Arrays.asList("two", "seven", " "))));
+    }
+
+    @Test
+    public void whenNoneElementsDeletedFromArrayThenReturnEmptyList() {
+        assertThat(strAnalyzer.findDeletedSlowly(sourceStrArr, anotherEditedStrArr), is(Collections.EMPTY_LIST));
+    }
+
+    @Test
+    public void whenFirstArrayIsEmptyThenReturnEmptyList() {
+        assertThat(strAnalyzer.findDeletedSlowly(zeroStrArr, sourceStrArr), is(Collections.EMPTY_LIST));
+    }
+
+    @Test
+    public void whenSecondArrayIsEmptyThenReturnEmptyList() {
+        assertThat(strAnalyzer.findDeletedSlowly(anotherEditedStrArr, zeroStrArr), is(Collections.EMPTY_LIST));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenFirstArrIsNullThenThrowIAException() {
+        strAnalyzer.findDeletedSlowly(null, sourceStrArr);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenSecondArrIsNullThenThrowIAException() {
+        strAnalyzer.findDeletedSlowly(sourceStrArr, null);
+    }
+
+
+    // Integer Tests
+    @Test
+    public void whenFindDeletedFrom10KArrayShouldReturnListWithDeleted() {
+        Long start = System.nanoTime();
+        intAnalyzer.findDeleted(src, edited);
+        Long finish = System.nanoTime();
+        System.out.println("@IntegerTests findDeleted: " + (finish - start) / 1000000 + " ms");
+    }
+
+    @Test
+    public void whenFindDeletedSlowFrom10KArrayShouldReturnListWithDeleted() {
+        Long start = System.nanoTime();
+        intAnalyzer.findDeletedSlowly(src, edited);
+        Long finish = System.nanoTime();
+        System.out.println("@IntegerTests findDeletedSlowly: " + (finish - start) / 1000000 + " ms");
+    }
+
+    // String Tests
+    @Test
+    public void whenFindFrom10KShouldReturnListWDeleted() {
+        Long start = System.nanoTime();
+        strAnalyzer.findDeleted(srcStr, editedStr);
+        Long finish = System.nanoTime();
+        System.out.println("@StringTests findDeleted: " + (finish - start) / 1000000 + " ms");
+    }
+
+    @Test
+    public void whenFindSlowFrom10KArrayShouldReturnListWDeleted() {
+        Long start = System.nanoTime();
+        strAnalyzer.findDeletedSlowly(srcStr, editedStr);
+        Long finish = System.nanoTime();
+        System.out.println("@StringTests findDeletedSlowly: " + (finish - start) / 1000000 + " ms");
     }
 }
