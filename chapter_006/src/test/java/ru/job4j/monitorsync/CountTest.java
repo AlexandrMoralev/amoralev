4066bd32b002b9940ee1089635afd457d3dfc7bf
AlexandrMoralev <moralev.alexandr@yandex.ru>
2018-12-19 19:41:25 +0300

    1. класс Count и метод int increment()[#78337]

---
+++ chapter_006/src/test/java/ru/job4j/monitorsync/CountTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.monitorsync;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * CountTest
+ *
+ * @author Alexandr Moralev (moralev.alexandr@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class CountTest {
+
+    private class ThreadCount extends Thread {
+        private final Count count;
+
+        private ThreadCount(final Count count) {
+            this.count = count;
+        }
+
+        @Override
+        public void run() {
+            this.count.increment();
+        }
+    }
+
+    @Test
+    public void whnExecute2ThreadThen2() throws InterruptedException {
+        final Count count = new Count();
+        Thread first = new ThreadCount(count);
+        Thread second = new ThreadCount(count);
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+        assertThat(count.get(), is(2));
+    }
+}
