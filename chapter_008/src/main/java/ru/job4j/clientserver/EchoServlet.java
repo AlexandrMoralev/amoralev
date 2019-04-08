aa4f022bdb2b51f07ad80e1436235f13f0e61cc9
AlexandrMoralev <moralev.alexandr@yandex.ru>
2019-04-08 22:18:38 +0300

    1. Создать web проект[#78299] - hello servlets

---
+++ chapter_008/src/main/java/ru/job4j/clientserver/EchoServlet.java
@@ -0,0 +1,23 @@
+package ru.job4j.clientserver;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class EchoServlet extends HttpServlet {
+    private static final Logger LOG = LoggerFactory.getLogger(EchoServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        writer.append("hello world");
+        writer.flush();
+    }
+}
