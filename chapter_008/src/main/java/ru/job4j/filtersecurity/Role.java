ab095a8a01afc75258c3c382b2a170a0d3594354
AlexandrMoralev <moralev.alexandr@yandex.ru>
2019-11-15 00:29:12 +0300

    Реализовать авторизация и аутентификацию[#78311] updated Role enum

--- chapter_008/src/main/java/ru/job4j/filtersecurity/Role.java
+++ chapter_008/src/main/java/ru/job4j/filtersecurity/Role.java
@@ -6,7 +6,7 @@ public enum  Role {
     MANAGER(2, "manager"),
     USER(3, "user"),
     MAINTENANCE(4, "maintenance"),
-    DEFAULT(5, "guest");
+    GUEST(5, "guest");
 
     private int index;
     private String description;
