247d6d18f268b0ec9176e68262d9666352288067
AlexandrMoralev <moralev.alexandr@yandex.ru>
2019-11-15 00:32:10 +0300

    Реализовать авторизацию и аутентификацию[#78311] Cleanup UserCreateController

--- chapter_008/src/main/java/ru/job4j/controllers/UserCreateController.java
+++ chapter_008/src/main/java/ru/job4j/controllers/UserCreateController.java
@@ -17,7 +17,6 @@ public class UserCreateController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        req.setAttribute("roles", Arrays.asList(Role.values()));
         req.getRequestDispatcher("/WEB-INF/views/user-create-view.jsp")
                 .forward(req, resp);
     }
