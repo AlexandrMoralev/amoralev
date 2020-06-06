16188c6942cc33687dc33129518de0dd105deb5c
AlexandrMoralev <moralev.alexandr@yandex.ru>
2020-06-06 18:38:34 +0300

    task_858 reading config

---
+++ chapter_009/src/main/java/ru/job4j/inputoutput/config/Config.java
@@ -0,0 +1,55 @@
+package ru.job4j.inputoutput.config;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class Config {
+    private final String path;
+    private final Map<String, String> values;
+
+    public Config(final String path) {
+        this.path = path.strip();
+        this.values = new HashMap<>();
+    }
+
+    public void load() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(this.path))) {
+            Consumer<String[]> putProperty = entry -> this.values.put(entry[0].strip(), entry[1].strip());
+            reader.lines()
+                    .map(String::strip)
+                    .filter(validLines)
+                    .map(v -> v.split("="))
+                    .filter(validKVPairs)
+                    .forEach(putProperty);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Predicate<String> validLines = v -> !v.isBlank() && !v.startsWith("#") && v.contains("=");
+    private Predicate<String[]> validKVPairs = arr -> arr.length == 2 && !arr[0].strip().isBlank() && !arr[1].strip().isBlank();
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        this.values.entrySet().stream().map(entry -> String.format("%s=%s", entry.getKey(), entry.getValue())).forEach(out::add);
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        Config config = new Config("app.properties");
+        config.load();
+        System.out.println(config);
+    }
+
+}
---
+++ chapter_009/src/test/java/ru/job4j/inputoutput/config/ConfigTest.java
@@ -0,0 +1,86 @@
+package ru.job4j.inputoutput.config;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class ConfigTest {
+
+    private static final Map<String, String> EXPECTED = Map.of(
+            "hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect",
+            "hibernate.connection.url", "jdbc:postgresql://127.0.0.1:5432/trackstudio",
+            "hibernate.connection.driver_class", "org.postgresql.Driver",
+            "hibernate.connection.username", "postgres",
+            "hibernate.connection.password", "password"
+    );
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./data/pairs_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertEquals(config.value("hibernate.dialect"), EXPECTED.get("hibernate.dialect"));
+        assertEquals(config.value("hibernate.connection.url"), EXPECTED.get("hibernate.connection.url"));
+        assertEquals(config.value("hibernate.connection.driver_class"), EXPECTED.get("hibernate.connection.driver_class"));
+        assertEquals(config.value("hibernate.connection.username"), EXPECTED.get("hibernate.connection.username"));
+        assertEquals(config.value("hibernate.connection.password"), EXPECTED.get("hibernate.connection.password"));
+
+    }
+
+    @Test
+    public void whenPairWithComment() {
+        String path = "./data/pairs_with_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertEquals(config.value("hibernate.dialect"), EXPECTED.get("hibernate.dialect"));
+        assertEquals(config.value("hibernate.connection.url"), EXPECTED.get("hibernate.connection.url"));
+
+        assertNull(config.value("hibernate.connection.driver_class"));
+
+        assertEquals(config.value("hibernate.connection.username"), EXPECTED.get("hibernate.connection.username"));
+        assertEquals(config.value("hibernate.connection.password"), EXPECTED.get("hibernate.connection.password"));
+    }
+
+    @Test
+    public void whenPairWithEmptyLines() {
+        String path = "./data/pairs_with_empty_lines.properties";
+        Config config = new Config(path);
+        config.load();
+        assertEquals(config.value("hibernate.dialect"), EXPECTED.get("hibernate.dialect"));
+        assertEquals(config.value("hibernate.connection.url"), EXPECTED.get("hibernate.connection.url"));
+        assertEquals(config.value("hibernate.connection.driver_class"), EXPECTED.get("hibernate.connection.driver_class"));
+        assertEquals(config.value("hibernate.connection.username"), EXPECTED.get("hibernate.connection.username"));
+        assertEquals(config.value("hibernate.connection.password"), EXPECTED.get("hibernate.connection.password"));
+    }
+
+    @Test
+    public void whenPairWithSpaces() {
+        String path = "./data/pairs_with_spaces.properties";
+        Config config = new Config(path);
+        config.load();
+        assertEquals(config.value("hibernate.dialect"), EXPECTED.get("hibernate.dialect"));
+        assertEquals(config.value("hibernate.connection.url"), EXPECTED.get("hibernate.connection.url"));
+        assertEquals(config.value("hibernate.connection.driver_class"), EXPECTED.get("hibernate.connection.driver_class"));
+        assertEquals(config.value("hibernate.connection.username"), EXPECTED.get("hibernate.connection.username"));
+        assertEquals(config.value("hibernate.connection.password"), EXPECTED.get("hibernate.connection.password"));
+    }
+
+    @Test
+    public void whenPairIncorrect() {
+        String path = "./data/incorrect_pairs.properties";
+        Config config = new Config(path);
+        config.load();
+        assertEquals(config.value("hibernate.dialect"), EXPECTED.get("hibernate.dialect"));
+        assertEquals(config.value("hibernate.connection.url"), EXPECTED.get("hibernate.connection.url"));
+        assertEquals(config.value("hibernate.connection.driver_class"), EXPECTED.get("hibernate.connection.driver_class"));
+        assertEquals(config.value("hibernate.connection.username"), EXPECTED.get("hibernate.connection.username"));
+        assertEquals(config.value("hibernate.connection.password"), EXPECTED.get("hibernate.connection.password"));
+        assertNull(config.value("hibernate"));
+        assertNull(config.value(""));
+
+    }
+
+}
