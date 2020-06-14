bb6b42b2506fe6ff25c20bb3b95055edb41829b7
AlexandrMoralev <moralev.alexandr@yandex.ru>
2020-06-15 00:47:33 +0300

    task_7921 bot

--- chapter_009/src/main/java/ru/job4j/socket/base/EchoServer.java
+++ chapter_009/src/main/java/ru/job4j/socket/base/EchoServer.java
@@ -7,29 +7,67 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.function.Predicate.not;
 
 public class EchoServer {
 
-    private static final String STOP = "msg=Bye";
+    private static final String EXIT = "Exit";
+    private static final String ACTION_PARAM = "msg";
 
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             boolean isRun = true;
+            List<String> requestInfo = new ArrayList<>();
             while (isRun) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
                     String str;
-                    List<String> requestInfo = new ArrayList<>();
                     while (!(str = in.readLine()).isEmpty()) {
                         requestInfo.add(str);
                         System.out.println(str);
                     }
-                    isRun = requestInfo.stream().noneMatch(s -> s.contains(STOP));
-                    out.write("HTTP/1.1 200 OK\r\n".getBytes());
+                    Map<String, String> requestParams = parseRequestParameters(requestInfo);
+                    requestInfo.clear();
+                    String answer = "";
+                    if (EXIT.equals(requestParams.get(ACTION_PARAM))) {
+                        isRun = false;
+                    } else {
+                        answer = requestParams.get(ACTION_PARAM);
+                    }
+                    out.write(String.format("HTTP/1.1 200 OK >> %s \r\n", answer).getBytes());
                 }
             }
         }
+    }
+
+    private static Map<String, String> parseRequestParameters(Collection<String> requestInfo) {
+        return requestInfo.stream()
+                .map(String::strip)
+                .filter(not(String::isBlank))
+                .filter(s -> s.contains("/?"))
+                .map(extractRequestParams())
+                .map(v -> v.split(","))
+                .flatMap(Stream::of)
+                .map(String::strip)
+                .filter(not(String::isBlank))
+                .map(s -> s.split("="))
+                .collect(
+                        Collectors.toMap(
+                                arr -> arr[0].replaceFirst("\\?", "").strip().toLowerCase(),
+                                arr -> arr[1].strip(),
+                                (existing, replacement) -> existing)
+                );
+    }
+
+    private static Function<String, String> extractRequestParams() {
+        return s -> s.split("\\?", -1)[1].strip().split("\\s+")[0].strip();
     }
 }
