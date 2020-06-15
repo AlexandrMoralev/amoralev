1a2c555ef0587e8c2e4179696f721e3bfcfb73b8
AlexandrMoralev <moralev.alexandr@yandex.ru>
2020-06-16 00:47:26 +0300

    task_268852 Slf4j - вывод переменных

--- chapter_009/src/main/java/ru/job4j/logging/UsageLog4j.java
+++ chapter_009/src/main/java/ru/job4j/logging/UsageLog4j.java
@@ -13,5 +13,16 @@ public class UsageLog4j {
         LOG.info("info message");
         LOG.warn("warn message");
         LOG.error("error message");
+
+        boolean isIs = true;
+        char symbol = '$';
+        byte bt = 0x2;
+        short shrt = 2;
+        int intgr = Integer.MIN_VALUE;
+        float flt = 2.222222f;
+        double dbl = 3.3000001d;
+        long lng = 100000000L;
+        LOG.debug("Logging primitives: boolean: {}, char: {}, byte: {}, short: {}, int: {}, float: {}, double: {}, long: {}",
+                isIs, symbol, bt, shrt, intgr, flt, dbl, lng);
     }
 }
