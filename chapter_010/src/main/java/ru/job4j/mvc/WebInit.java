0a513de5ef6adfa40964e0ff865e878d31ddacf3
AlexandrMoralev <moralev.alexandr@yandex.ru>
2020-07-08 19:40:48 +0300

    task_254391 mvc base

---
+++ chapter_010/src/main/java/ru/job4j/mvc/WebInit.java
@@ -0,0 +1,23 @@
+package ru.job4j.mvc;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class WebInit implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(WebInit.class);
+        ctx.refresh();
+        DispatcherServlet dispatcher = new DispatcherServlet(ctx);
+        ServletRegistration.Dynamic registration = servletContext.addServlet("app", dispatcher);
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/");
+    }
+}
---
+++ chapter_010/src/main/java/ru/job4j/mvc/control/IndexControl.java
@@ -0,0 +1,14 @@
+package ru.job4j.mvc.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexControl {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
+}
